c83c95b22a687fc4a92df6a98ab398fc08efca7b
moseskamira <moses.african@gmail.com>
2019-03-22 13:47:29 +0300

    feat(GithubUserAdapter): create the GithubUserAdapter for the application
    Create the GithubUserAdapter for the application
    [Maintains #155965773]

--- app/src/main/java/com/example/androidcodelabapp/MainActivity.java
+++ app/src/main/java/com/example/androidcodelabapp/MainActivity.java
@@ -5,19 +5,42 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
-
+    private ArrayList <String> userNames = new ArrayList<>();
+    private ArrayList <String> imageUrls = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initImageBitmaps();
+        initRecyclerViewAdapter();
+
+
+    }
+    private void initImageBitmaps(){
+        imageUrls.add("https://homepages.cae.wisc.edu/~ece533/images/airplane.png");
+        userNames.add("moseskamira");
+        imageUrls.add("https://homepages.cae.wisc.edu/~ece533/images/boat.png");
+        userNames.add("evamaina");
+        imageUrls.add("https://homepages.cae.wisc.edu/~ece533/images/baboon.png");
+        userNames.add("emmanuelomona");
+
+
+
+
+    }
+    private void initRecyclerViewAdapter(){
         recyclerView = findViewById(R.id.recyclerview);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, userNames, imageUrls);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
 
     }
 
